94f9f9230b84e33e15e4cc7f5418dd4e75d12282
up202004187 <up202004187@up.pt>
2023-09-22 20:12:26 +0100

    database ready

---
+++ db/run.sql
@@ -0,0 +1,9 @@
+.open ac.db
+.mode csv
+.import awards_players.csv awards_players
+.import coaches.csv coaches
+.import players_teams.csv players_teams
+.import players.csv players
+.import series_post.csv series_post
+.import teams_post.csv teams_post
+.import teams.csv teams
