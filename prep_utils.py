6650c554a2e28b984081e690ba093626cc071c5b
oliveira002 <deadzin112@gmail.com>
2023-09-23 12:25:14 +0100

    Started Data Preparation. SQL Queries for N/A & Descriptive Statistics

---
+++ prep_utils.py
@@ -0,0 +1,61 @@
+from tabulate import tabulate
+
+def parse_columns_types(db_cur,attributes, table):
+    numerical = []
+    non_numerical = []
+    for attr in attributes:
+        query = f"SELECT typeof({attr}) FROM {table} LIMIT 1;"
+        db_cur.execute(query)
+        result = db_cur.fetchone()[0]
+        if(result == "integer" or result == "real"):
+            numerical.append(attr)
+        else:
+            non_numerical.append(attr)
+    
+    return numerical,non_numerical
+        
+
+
+def check_missing_values(db_cur, attributes, table):
+    missing_values = {}
+
+    for column_name in attributes:
+        query = f"SELECT COUNT(*) FROM {table} WHERE {column_name} IS NULL;"
+        db_cur.execute(query)
+        result = db_cur.fetchone()[0]
+        missing_values[column_name] = result > 0
+
+
+    # Get all coaches from database
+    for column, has_missing in missing_values.items():
+        print(f"Column '{column}' has missing values: {has_missing}")
+
+def calculate_summary_statistics(db_cur, table, numerical_columns):
+    summary_stats = []
+
+    for column in numerical_columns:
+        query = f"""
+            SELECT
+                COUNT({column}) AS count,
+                AVG({column}) AS mean,
+                ROUND(SUM(({column} - (
+                    SELECT AVG({column}) FROM {table}
+                )) * ({column} - (
+                    SELECT AVG({column}) FROM {table}
+                ))) / (COUNT({column}) - 1), 2) AS std,
+                MIN({column}) AS min,
+                MAX({column}) AS max
+            FROM {table}
+        """
+        
+        db_cur.execute(query)
+        result = db_cur.fetchone()
+        
+        stats = [column] + list(result)
+        summary_stats.append(stats)
+
+    # Define the headers for the table
+    headers = ['Attribute', 'Count', 'Mean', 'Std Deviation', 'Min', 'Max']
+
+    # Print the summary statistics as a table
+    print(tabulate(summary_stats, headers, tablefmt="grid"))
