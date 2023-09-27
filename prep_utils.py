6b3ca52c4f4298a5dbfe51b63e2ae280c6a92626
oliveira002 <deadzin112@gmail.com>
2023-09-27 19:33:42 +0100

    Data Exploration

--- prep_utils.py
+++ prep_utils.py
@@ -1,6 +1,15 @@
 from tabulate import tabulate
 
-def parse_columns_types(db_cur,attributes, table):
+
+def get_db_tables(db_cur):
+    query = f"SELECT name FROM sqlite_master WHERE type='table';"
+    db_cur.execute(query)
+    result = db_cur.fetchall()
+    return [x[0] for x in result]
+
+
+def parse_columns_type(db_cur, table):
+    attributes = get_table_attributes(db_cur,table)
     numerical = []
     non_numerical = []
     for attr in attributes:
@@ -13,10 +22,16 @@ def parse_columns_types(db_cur,attributes, table):
             non_numerical.append(attr)
     
     return numerical,non_numerical
-        
+
+def get_table_attributes(db_cur, table):
+    query = f"select name from pragma_table_info('{table}');"
+    db_cur.execute(query)
+    result = db_cur.fetchall()
+    return [x[0] for x in result]
 
 
-def check_missing_values(db_cur, attributes, table):
+def check_missing_values(db_cur, table):
+    attributes = get_table_attributes(db_cur,table)
     missing_values = {}
 
     for column_name in attributes:
