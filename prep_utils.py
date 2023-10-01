962e8e8125fe664728e101d8f33d6b5a4aa4e279
oliveira002 <deadzin112@gmail.com>
2023-10-01 20:15:25 +0100

    Fix minor errors

--- prep_utils.py
+++ prep_utils.py
@@ -31,19 +31,33 @@ def get_table_attributes(db_cur, table):
 
 
 def check_missing_values(db_cur, table):
-    attributes = get_table_attributes(db_cur,table)
+    attributes = get_table_attributes(db_cur, table)
     missing_values = {}
 
     for column_name in attributes:
+        total_query = f"SELECT COUNT(*) FROM {table};"
+        db_cur.execute(total_query)
+        total_rows = db_cur.fetchone()[0]
+
         query = f"SELECT COUNT(*) FROM {table} WHERE {column_name} IS NULL OR {column_name} = '';"
         db_cur.execute(query)
-        result = db_cur.fetchone()[0]
-        missing_values[column_name] = result > 0
+        null_count = db_cur.fetchone()[0]
+        percentage_null = (null_count / total_rows) * 100
+        
+        missing_values[column_name] = {
+            'has_missing': null_count > 0,
+            'percentage_null': percentage_null
+        }
 
+    flag = False
 
-    # Get all coaches from database
-    for column, has_missing in missing_values.items():
-        print(f"Column '{column}' has missing values: {has_missing}")
+    for column, values in missing_values.items():
+        if(values['has_missing']):
+            flag = True
+            print(f"Column '{column}' has missing values: {values['has_missing']} - {values['percentage_null']:.2f}%")
+    
+    if not flag:
+        print("There are no missing values!",end="")
 
 def calculate_summary_statistics(db_cur, table, numerical_columns):
     summary_stats = []
@@ -53,11 +67,11 @@ def calculate_summary_statistics(db_cur, table, numerical_columns):
             SELECT
                 COUNT({column}) AS count,
                 AVG({column}) AS mean,
-                ROUND(SUM(({column} - (
+                ROUND(POWER(SUM(({column} - (
                     SELECT AVG({column}) FROM {table}
                 )) * ({column} - (
                     SELECT AVG({column}) FROM {table}
-                ))) / (COUNT({column}) - 1), 2) AS std,
+                ))) / (COUNT({column}) - 1), 0.5), 2) AS std,
                 MIN({column}) AS min,
                 MAX({column}) AS max
             FROM {table}
